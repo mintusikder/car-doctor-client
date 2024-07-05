d6216d3e56b68746364dda8a8e8fcd835e74ece6
Mintu Sikder <mintusikder15@gmail.com>
2024-07-05 19:04:26 +0600

    added log Out

--- src/pages/Provider/AuthProvider.jsx
+++ src/pages/Provider/AuthProvider.jsx
@@ -4,6 +4,7 @@ import {
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import app from "../Firebase/firebase.config";
 export const AuthContext = createContext();
@@ -22,6 +23,11 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  const logOut = () =>{
+    setLoading(true)
+    return signOut(auth)
+  }
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -38,6 +44,7 @@ const AuthProvider = ({ children }) => {
     loading,
     createUser,
     login,
+    logOut,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,52 +1,80 @@
 import { Link } from "react-router-dom";
-import logo from "../../../assets/logo.svg"
+import logo from "../../../assets/logo.svg";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import { signOut } from "firebase/auth";
 
 const Navbar = () => {
-    const navItems =(
-        <>
-         <li><Link to="/">Home</Link></li>
-         <li><Link to="/about">About</Link></li>
-         <li><Link to="/service">Service</Link></li>
-        </>
-    )
-    return (
-        <div className="navbar bg-base-100 h-28 mb-4">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16" />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            {navItems}
-            </ul>
+  const { user, logOut } = useContext(AuthContext);
+  const handelLogout = () => {
+    logOut()
+      .then(() =>{})
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  const navItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/service">Service</Link>
+      </li>
+
+      {user?.email ? (
+        <li>
+          <button onClick={handelLogout}>Log Out</button>
+        </li>
+      ) : (
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      )}
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 h-28 mb-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <Link className="btn btn-ghost text-xl">
-          <img src={logo} alt="" />
-          </Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-          {navItems}
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {navItems}
           </ul>
         </div>
-        <div className="navbar-end">
-        <button className="btn btn-outline btn-warning">Appointment</button>
-        </div>
+        <Link className="btn btn-ghost text-xl">
+          <img src={logo} alt="" />
+        </Link>
       </div>
-    );
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-outline btn-warning">Appointment</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
